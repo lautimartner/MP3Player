3d7d32402ba25c4b7e8a651ddfc4445059c48df0
lautimartner <lautimartner@ciencias.unam.mx>
2018-10-17 20:39:21 -0500

    Todo listo para que se pueda crear un ejecutable

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-#! /usr/bin/env python
+#! /usr/bin/env python3
 #! /usr/bin/python3.6
 # Form implementation generated from reading ui file 'untitled.ui'
 # Created by: PyQt5 UI code generator 5.10.1
